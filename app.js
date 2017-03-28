77701a9bd61acb4209ccc396c5c184646da0270d
Alvin Chong <alvinchong92@gmail.com>
2017-03-27 21:47:43 -0400

    making it better

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 console.log("loaded")
 
-function myFunction() {
-  // console.log("linked")
-    let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += "responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
 $(document).ready(function() {
   $("#name").slideUp(1).delay("1000").slideDown('slow');
   $("#content").slideUp(1).delay("2000").slideDown('slow');
   })
-
-
-// $(document).ready(function() {
-//   $("#name").slideDown("4000", function() {
-//     $("#name").css("visibility", "hidden" );
-//   });
-// })
