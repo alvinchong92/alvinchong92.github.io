1766c9dc56bb9bb2bb23b253b3319ccd5b6f2821
Alvin Chong <alvinchong92@gmail.com>
2017-04-14 20:28:33 -0400

    responsive feel

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("loaded")
 
 $(document).ready(function() {
-  $("#name").slideUp(1).delay("1000").slideDown('slow');
-  $("#content").slideUp(1).delay("2000").slideDown('slow');
+  $(".header__title").slideUp(1).delay("1000").slideDown('slow');
+  $(".content").slideUp(1).delay("2000").slideDown('slow');
   })
